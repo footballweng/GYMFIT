d671b5d22ae12d6b742cedae226964ce91cd0223
taterazzy <taterazzy@gmail.com>
2016-11-17 17:21:48 +0800

    Revert "Revert "DIVCEPTION""
    This reverts commit 3b9672b92ce744b5049f3dba8bed48226ee1e640.

--- GYMFIT/venueBooking.aspx.designer.cs
+++ GYMFIT/venueBooking.aspx.designer.cs
@@ -22,33 +22,6 @@ namespace GYMFIT {
         protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
         
         /// <summary>
-        /// DropDownList4 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList DropDownList4;
-        
-        /// <summary>
-        /// DropDownList2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList DropDownList2;
-        
-        /// <summary>
-        /// DropDownList3 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList DropDownList3;
-        
-        /// <summary>
         /// TextBox1 control.
         /// </summary>
         /// <remarks>
